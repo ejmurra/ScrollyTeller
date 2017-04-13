51bc9a04bec5718740f4e035a304f3e0f301ba4c
Eli Murray <emurray@tampabay.com>
2017-04-13 12:01:07 -0400

    uses setters to update styles

--- src/ScrollyTeller.ts
+++ src/ScrollyTeller.ts
@@ -1,103 +1,158 @@
 import { EventEmitter } from "./EventEmitter";
 
 export class ScrollyTeller {
-    graphs: string[];
-    anchorRoot: HTMLElement;
-    graphicRoot: HTMLElement;
-    scrollCover: HTMLElement;
-    contentWell: HTMLElement;
-    events: EventEmitter<any> = new EventEmitter();
+  graphs: string[];
+  anchorRoot: HTMLElement;
+  graphicRoot: HTMLElement;
+  scrollCover: HTMLElement;
+  contentWell: HTMLElement;
+  events: EventEmitter<any> = new EventEmitter();
+  private _graphMargin: string;
 
-    constructor(root: HTMLElement | string, graphs: string[], opts: any) {
-        if (!root || !graphs) {
-            throw new Error(`Class ScrollyTeller is missing required arguments. Please specify a root element and graphs of text.
+  private graphicRootActiveStyles: { [prop: string]: string };
+  private contentWellActiveStyles: { [prop: string]: string };
+  private graphActiveStyles: { [prop: string]: string };
+  private scrollCoverActiveStyles: { [prop: string]: string };
+
+  private graphicRootDefaultStyles = {
+    "background-color": "inherit",
+    "width": "100vw",
+    "height": "100vh",
+    "position": "fixed",
+    "top": "0",
+    "left": "0",
+    "z-index": "-1",
+    "-webkit-transition": "background-color 2s",
+    "transition": "background-color 2s"
+  };
+
+  private contentWellDefaultStyles = {
+    "max-width": "700px",
+    "width": "100vw"
+  };
+
+  private graphDefaultStyles = {};
+
+  private scrollCoverDefaultStyles = {
+    "width": "100vw",
+    "z-index": "5"
+  };
+
+  get graphMargin() {
+    return this._graphMargin || "40vh";
+  }
+
+  set graphMargin(margin: string) {
+    this._graphMargin = margin;
+    const graphChildren = Array.prototype.slice.call(this.contentWell.querySelectorAll("p"));
+    for (let childEl of graphChildren) {
+      const margins = {
+        "margin-top": this._graphMargin,
+        "margin-bottom": graphChildren.indexOf(childEl) === graphChildren.length - 1 ? this._graphMargin : "inherit"
+      };
+      childEl.style.cssText = this.styleObjToString(Object.assign({}, this.graphActiveStyles, margins))
+    }
+  }
+
+  set graphicRootStyles(styles: { [prop: string]: string }) {
+    this.graphicRootActiveStyles = Object.assign({}, this.graphicRootDefaultStyles, this.graphicRootActiveStyles,
+      styles);
+    this.graphicRoot.style.cssText = this.styleObjToString(this.graphicRootActiveStyles);
+  }
+
+  set contentWellStyles(styles: { [prop: string]: string }) {
+    this.contentWellActiveStyles = Object.assign({}, this.contentWellDefaultStyles, this.contentWellActiveStyles,
+      styles);
+    this.contentWell.style.cssText = this.styleObjToString(this.contentWellActiveStyles);
+  }
+
+  set graphStyles(styles: { [prop: string]: string }) {
+    this.graphActiveStyles = Object.assign({}, this.graphDefaultStyles, this.graphActiveStyles, styles);
+    const graphChildren = Array.prototype.slice.call(this.contentWell.querySelectorAll("p"));
+    for (let childEl of graphChildren) {
+      const margins = {
+        "margin-top": this.graphMargin,
+        "margin-bottom": graphChildren.indexOf(childEl) === graphChildren.length - 1 ? this.graphMargin : "inherit"
+      };
+      childEl.style.cssText = this.styleObjToString(Object.assign({}, this.graphActiveStyles, margins));
+    }
+  }
+
+  set scrollCoverStyles(styles: { [prop: string]: string }) {
+    this.scrollCoverActiveStyles = Object.assign({}, this.scrollCoverDefaultStyles, this.scrollCoverActiveStyles,
+      styles);
+    this.scrollCover.style.cssText = this.styleObjToString(this.scrollCoverActiveStyles);
+  }
+
+  constructor(root: HTMLElement | string, graphs: string[], opts?: { graphMargin?: string }) {
+    if (!root || !graphs) {
+      throw new Error(`Class ScrollyTeller is missing required arguments. Please specify a root element and graphs of text.
                 Given: {
                     root: ${root},
                     graphs: ${graphs}
                 }`);
-        }
-
-        this.anchorRoot = (root && typeof root === 'string' ? <HTMLElement><any>document.querySelector(root) :
-                          <HTMLElement>root);
-        this.graphs = graphs;
-        this.graphicRoot = this._createGraphicRoot(opts && opts.graphicRootStyles ? opts.graphicRootStyles : {});
-        this.scrollCover = this._createScrollCover(opts && opts.scrollCoverStyles ? opts.scrollCoverStyles : {});
-        this.contentWell = this._createContentWell(opts && opts.contentWellStyles ? opts.contentWellStyles: {},
-                                                   opts && opts.graphMargin ? opts.graphMargin : "40vh");
-        this.anchorRoot.appendChild(<Node><any>this.graphicRoot);
-        this.scrollCover.appendChild(<Node><any>this.contentWell);
-        this.anchorRoot.appendChild(<Node><any>this.scrollCover);
     }
+    this.anchorRoot = (typeof root === 'string' ? <HTMLElement>document.querySelector(root) : <HTMLElement>root);
+    this.graphs = graphs;
+    this.graphicRoot = this.createGraphicRoot();
+    this.scrollCover = this.createScrollCover();
+    this.contentWell = this.createContentWell();
+    this.anchorRoot.appendChild(this.graphicRoot);
+    this.scrollCover.appendChild(this.contentWell);
+    this.anchorRoot.appendChild(this.scrollCover);
+  }
 
-    activate(color: string): ScrollyTeller {
-        this.graphicRoot.style.backgroundColor = color || "#0e1d1d";
-        this.events.emit({ event: "activated" });
-        return this;
-    }
 
-    deactivate(color: string): ScrollyTeller {
-        this.graphicRoot.style.backgroundColor = color || "inherit";
-        this.events.emit({ event: "deactivated" });
-        return this;
-    }
+  activate(color: string): ScrollyTeller {
+    this.graphicRootStyles = { "background-color": color || "#0e1d1d" };
+    this.events.emit({event: "activated"});
+    return this;
+  }
 
-    _createContentWell(customStyles: any, graphMargin: string): HTMLElement {
-        const defaults = {
-            "max-width": "700px",
-            "width": "100vw"
-        };
-        const styles = (<any>Object).assign({}, defaults, customStyles);
-        const contentWell = <HTMLElement><any>document.createElement("div");
-        contentWell.style.cssText = this._styleObjToString(styles);
-        for (let graph of this.graphs) {
-            const idx = this.graphs.indexOf(graph);
-            let p = <HTMLElement><any>document.createElement("p");
-            p.style.marginTop = graphMargin;
-            if (idx === this.graphs.length -1) p.style.marginBottom = graphMargin;
-            p.innerText = graph;
-            p.id = `scrolly-teller-graph-${idx}`;
-            p.classList.add("scrolly-teller-graph");
-            contentWell.appendChild(<Node><any>p);
-        }
-        return contentWell;
-    }
+  deactivate(color: string): ScrollyTeller {
+    this.graphicRootStyles = { "background-color": color || "#0e1d1d" };
+    this.events.emit({event: "deactivated"});
+    return this;
+  }
 
-    _createScrollCover(customStyles: any): HTMLElement {
-        const defaults = {
-            "width": "100vw",
-            "z-index": "5"
-        };
-        const styles = (<any>Object).assign({}, defaults, customStyles);
-        const scrollCover = <HTMLElement><any>document.createElement("div");
-        scrollCover.style.cssText = this._styleObjToString(styles);
-        return scrollCover;
+  private createContentWell(): HTMLElement {
+    const contentWell = document.createElement("div");
+    contentWell.style.cssText = this.styleObjToString(this.contentWellDefaultStyles);
+    for (let graph of this.graphs) {
+      const idx = this.graphs.indexOf(graph);
+      let p = document.createElement("p");
+      p.style.cssText = this.styleObjToString(this.graphDefaultStyles);
+      p.style.marginTop = this.graphMargin;
+      if (idx === this.graphs.length - 1) {
+        p.style.marginBottom = this.graphMargin;
+      }
+      p.innerText = graph;
+      p.id = `scrolly-teller-graph-${idx}`;
+      p.classList.add("scrolly-teller-graph");
+      contentWell.appendChild(p);
     }
+    return contentWell;
+  }
 
-    _createGraphicRoot(customStyles: any): HTMLElement {
-        const defaults = {
-            "background-color": "inherit",
-            "width": "100vw",
-            "height": "100vh",
-            "position": "fixed",
-            "top": "0",
-            "left": "0",
-            "z-index": "-1",
-            "-webkit-transition": "background-color 2s",
-            "transition": "background-color 2s"
-        };
-        const styles = (<any>Object).assign({}, defaults, customStyles);
-        const graphicRoot = <HTMLElement><any>document.createElement("div");
-        graphicRoot.style.cssText = this._styleObjToString(styles);
-        return graphicRoot;
-    }
+  private createScrollCover(): HTMLElement {
+    const scrollCover = document.createElement("div");
+    scrollCover.style.cssText = this.styleObjToString(this.scrollCoverDefaultStyles);
+    return scrollCover;
+  }
 
-    _styleObjToString(styleObj: any): string {
-        let str = "";
-        for (let prop in styleObj) {
-            if (styleObj.hasOwnProperty(prop)) {
-                str += `${prop}: ${styleObj[prop]};`
-            }
-        }
-        return str;
+  private createGraphicRoot(): HTMLElement {
+    const graphicRoot = document.createElement("div");
+    graphicRoot.style.cssText = this.styleObjToString(this.graphicRootDefaultStyles);
+    return graphicRoot;
+  }
+
+  private styleObjToString(styleObj: any): string {
+    let str = "";
+    for (let prop in styleObj) {
+      if (styleObj.hasOwnProperty(prop)) {
+        str += `${prop}: ${styleObj[prop]};`
+      }
     }
+    return str;
+  }
 }
