89521955f367fae2ea47ccf7cee67287878baf32
Eli Murray <emurray@tampabay.com>
2017-04-14 11:33:24 -0400

    bundled definitions for npm

---
+++ defs/ScrollyTeller.d.ts
@@ -0,0 +1,44 @@
+import { EventEmitter } from "./EventEmitter";
+export declare class ScrollyTeller {
+    graphs: string[];
+    anchorRoot: HTMLElement;
+    graphicRoot: HTMLElement;
+    scrollCover: HTMLElement;
+    contentWell: HTMLElement;
+    events: EventEmitter<any>;
+    private _graphMargin;
+    private ticking;
+    private graphChildren;
+    private graphicRootActiveStyles;
+    private contentWellActiveStyles;
+    private graphActiveStyles;
+    private scrollCoverActiveStyles;
+    private graphicRootDefaultStyles;
+    private contentWellDefaultStyles;
+    private graphDefaultStyles;
+    private scrollCoverDefaultStyles;
+    graphMargin: string;
+    graphicRootStyles: {
+        [prop: string]: string;
+    };
+    contentWellStyles: {
+        [prop: string]: string;
+    };
+    graphStyles: {
+        [prop: string]: string;
+    };
+    scrollCoverStyles: {
+        [prop: string]: string;
+    };
+    constructor(root: HTMLElement | string, graphs: string[], opts?: {
+        graphMargin?: string;
+    });
+    activate(color: string): ScrollyTeller;
+    deactivate(color: string): ScrollyTeller;
+    private scrollEmitterFunction();
+    private scrollThrottler(position);
+    private createContentWell();
+    private createScrollCover();
+    private createGraphicRoot();
+    private styleObjToString(styleObj);
+}
